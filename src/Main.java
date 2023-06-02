186f1d18643bdf96d737497f132ad377503b26f0
RM87107 <okarencristiny0210@gmail.com>
2023-06-02 09:59:36 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Pessoa obj1 = new Pessoa();
+        obj1.nome = "Karen Cristiny ";
+        obj1.cpf = "321.745.698-19" ;
+        obj1.rg ="31.543.879-x";
+        obj1.endereço =" Rua Noel Rosa -21";
+        obj1.telefone = " 1197632-4431" ;
+        obj1.telefoneEmergencia = " 1198542-8990";
+
+
+
+
+
+
+    }
+}
---
+++ src/Pessoa.java
@@ -0,0 +1,13 @@
+public class Pessoa {
+
+    //Declaração de objetos
+
+    String nome;
+    String cpf;
+    String rg;
+    String telefone;
+    String telefoneEmergencia;
+    String endereço;
+
+
+}

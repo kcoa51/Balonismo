7885301c4ba8790b91f42726c4da51c062001c3d
Karen Cristiny <okarencristiny0210@gmail.com>
2023-08-10 10:46:50 -0300

    Atividade Balonismo

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,12 @@ public class Main {
         Balão b3 = new Balão(2,"rio","roxo","quadrado",55.30,10.0,"grande",4);
 
         // Construindo pessoa
-        Pessoa p1 = new Pessoa("Karen",97612365710,795435,11976355215,21985922219,"Rua Noel Rosa ");
+        Pessoa p1 = new Pessoa("Karen",97612365,795435,1197635521,21922219,"Rua Noel Rosa ");
+        Pessoa p2 = new Pessoa("Lira",965364789,867849,31985919,41934332,"Rua Tom Jobim");
+
+        // Construindo piloto
+        Piloto pi1= new Piloto("Joaquim",421876456, 5432);
+        Piloto pi2= new Piloto("Anderson",853213987, 2106);
     }
 
 }
--- src/Piloto.java
+++ src/Piloto.java
@@ -1,3 +1,37 @@
 public class Piloto {
 
+    // Atributos
+    private String nome;
+    private int cpf;
+    private int numAc;
+
+    public Piloto(String nome, int cpf, int numAc) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.numAc = numAc;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+
+    public int getNumAc() {
+        return numAc;
+    }
+
+    public void setNumAc(int numAc) {
+        this.numAc = numAc;
+    }
 }
